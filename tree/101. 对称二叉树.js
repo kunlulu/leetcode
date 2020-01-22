f0840f2c11487afa600dbf9f0b56559420f35b74
kunlulu <59993024+kunlulu@users.noreply.github.com>
2020-01-22 15:45:44 +0800

    Create 101. 对称二叉树.js

---
+++ tree/101. 对称二叉树.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+  const isSymmetricTree = (n1,n2) => {
+      if(!n1 && !n2) return true
+      if(!n1 || !n2) return false
+      if(n1.val != n2.val) return false
+      return isSymmetricTree(n1.left,n2.right) && isSymmetricTree(n1.right,n2.left)
+  }
+  return isSymmetricTree(root,root)
+};
